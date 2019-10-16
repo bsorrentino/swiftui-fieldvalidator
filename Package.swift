ffba1772cfe27d144dfe27ef380aaf7341135aea
bsorrentino <bartolomeo.sorrentino@gmail.com>
2019-10-16 17:19:17 +0200

    add iOS v9 platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "FieldValidatorLibrary",
+    platforms: [
+        //.macOS(.v10_10),
+        //.tvOS(.v9),
+        //.watchOS(.v2),
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
