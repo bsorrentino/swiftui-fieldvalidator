8ddd216be04697c032a42788baf36baa3f95e69e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2019-11-04 23:19:38 +0100

    update

--- Package.swift
+++ Package.swift
@@ -26,10 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FieldValidatorLibrary",
-            dependencies: [])
-        //,
-        //.testTarget(
-        //    name: "FieldValidatorLibraryTests",
-        //    dependencies: ["FieldValidatorLibrary"]),
+            dependencies: []),
+        .testTarget(
+            name: "FieldValidatorLibraryTests",
+            dependencies: ["FieldValidatorLibrary"]),
     ]
 )
