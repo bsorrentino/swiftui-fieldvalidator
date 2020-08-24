ea548a8f46bcc91f75592debe505d53b0a84e7da
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-08-24 13:17:07 +0200

    update assets

--- assets/mov_to_gif.sh
+++ assets/mov_to_gif.sh
@@ -2,4 +2,4 @@
 # https://gist.github.com/SheldonWangRJT/8d3f44a35c8d1386a396b9b49b43c385
 #
 
-ffmpeg -i FieldValidatorLibrary.mov -pix_fmt rgb8 -r 10 FieldValidatorLibrary.gif
+ffmpeg -i FieldValidatorLibrarySample.mov -pix_fmt rgb8 -r 10 FieldValidatorLibrarySAmple.gif
